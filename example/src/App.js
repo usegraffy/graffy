b3321428c489006ae0e6a88fd81c347871f1c764
Aravind R S <aravind@zendesk.com>
2018-10-04 00:19:55 +0800

    Created example app

---
+++ example/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Grue } from 'grue/core';
+import { Cache } from 'grue/cache';
+
+import './App.css';
+
+const g = new Grue();
+g.use(new Cache());
+
+class App extends Component {
+  state = { pokes: {} }
+
+  handleClick = () => {
+    const id = `p${Date.now()}`;
+    g.put(['pokes', id], true);
+  }
+
+  handleUpdate = (value) => {
+    this.setState(value);
+  }
+
+  componentDidMount() {
+    for await (let value of g.on(['pokes']))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleClick}>Send</button>
+        <pre>{Object.keys(this.state.pokes).join('\n')}</pre>
+      </div>
+    );
+  }
+}
+
+export default App;
