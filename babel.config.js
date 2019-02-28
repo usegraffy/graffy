3badcc2c526188c577e290de8491dd28904982fb
Aravind <aravindet@gmail.com>
2019-03-01 05:35:52 +0800

    Build script (wip)

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'presets': ['@babel/preset-env'],
+  'presets': [[ '@babel/preset-env', { loose: true } ]],
   'plugins': [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties'
