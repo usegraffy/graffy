7c2dab58917ea4cfac6c2121a37c3b393e2607c5
Aravind <aravindet@gmail.com>
2019-09-07 13:25:58 +0800

    Upgrade travis node version

--- src/common/encode/string.js
+++ src/common/encode/string.js
@@ -1,4 +1,10 @@
-import '@sinonjs/text-encoding';
+(function(g) {
+  if (!g.TextEncoder || !g.TextDecoder) {
+    const { TextEncoderLite, TextDecoderLite } = require('text-encoder-lite');
+    g.TextEncoder = TextEncoderLite;
+    g.TextDecoder = TextDecoderLite;
+  }
+})(global || window || this);
 
 const textEncoder = new TextEncoder();
 const textDecoder = new TextDecoder('utf-8');
