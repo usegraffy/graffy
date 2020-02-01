7eac4c5af861c1399219eb51b80febb3695a5baf
Aravind Ravi Sulekha <hi@aravind.rs>
2020-02-01 15:58:17 +0800

    Add final mode to cache
    This allows the cache to act as an authoritative, in-memory store

--- src/cache/index.js
+++ src/cache/index.js
@@ -1,17 +1,38 @@
-import { merge, slice } from '@graffy/common';
+import { merge, slice, setVersion } from '@graffy/common';
+import makeStream from '@graffy/stream';
+// import { debug } from '@graffy/testing';
 
-export default function() {
+export default function({ final } = {}) {
   return store => {
-    const cache = [];
+    const state = final ? [{ key: '', end: '\uffff', version: 0 }] : [];
+    const listeners = new Set();
 
     store.on('read', [], async (query, options, next) => {
       if (options.skipCache) return next(query);
-      const { known, unknown } = slice(cache, query);
+      const { known, unknown } = slice(state, query);
+      if (final) return setVersion(known, Date.now());
       if (!unknown) return known;
 
       const nextValue = await next(unknown);
-      merge(cache, nextValue);
+      merge(state, nextValue);
       return merge(known || [], nextValue);
+    });
+
+    store.on('watch', [], (query, options, next) => {
+      if (!final) return next();
+
+      return makeStream((push, _end) => {
+        push(undefined);
+        listeners.add(push);
+        return () => listeners.delete(push);
+      });
+    });
+
+    store.on('write', [], async (change, options, next) => {
+      const appliedChange = final ? change : await next(change);
+      merge(state, appliedChange);
+      for (const push of listeners) push(appliedChange);
+      return appliedChange;
     });
   };
 }
