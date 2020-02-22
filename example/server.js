d5b6e557c019c0db4ff6db672310985f66b16864
Aravind Ravi Sulekha <hi@aravind.rs>
2020-02-23 01:04:17 +0800

    API Cleanup

--- example/server.js
+++ example/server.js
@@ -3,17 +3,16 @@ import { fileURLToPath } from 'url';
 import express from 'express';
 
 import Graffy from '@graffy/core';
-import GraffyFill from '@graffy/fill';
-import GraffyCache from '@graffy/cache';
-import GraffyServer from '@graffy/server';
-import GraffyWsServer from '@graffy/server/wsServer';
+import fill from '@graffy/fill';
+import cache from '@graffy/cache';
+import { httpServer, wsServer } from '@graffy/server';
 import mock from './mockVisitorList';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const store = new Graffy();
-store.use(GraffyFill());
-store.use(GraffyCache({ final: true }));
+store.use(fill());
+store.use(cache({ final: true }));
 store.use(mock);
 
 const app = express();
@@ -28,11 +27,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api', GraffyServer(store));
+app.use('/api', httpServer(store));
 app.use(express.static(__dirname + '/public'));
 const server = app.listen(8443);
 
-server.on('upgrade', GraffyWsServer(store));
+server.on('upgrade', wsServer(store));
 
 // eslint-disable-next-line no-console
 console.log('Server started at 8443');
