4f71d3ef97ad2b1af0efd9d9d5495ec3d07786c5
Nisheet Sinvhal <nisheet1.sinvhal@gmail.com>
2020-10-21 14:06:46 +0530

    add base test cases for useQuery

---
+++ src/react/react.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Graffy from '@graffy/core';
+import { useQuery } from './';
+import { mockBackend } from '@graffy/testing';
+import { GraffyProvider } from './GraffyContext';
+import { renderHook } from '@testing-library/react-hooks';
+
+describe('useQuery', () => {
+  let g;
+  let wrapper;
+  let backend;
+
+  beforeEach(() => {
+    g = new Graffy();
+    backend = mockBackend();
+    backend.read = jest.fn(backend.read);
+    g.use(backend.middleware);
+    wrapper = ({ children }) => (
+      <GraffyProvider store={g}>{children}</GraffyProvider>
+    );
+  });
+
+  it('should transition loading as "true -> false" cycle on data fetch', async () => {
+    const value = 'abcde';
+    await g.write('/demo', { value });
+    const { result, waitForValueToChange } = renderHook(
+      () => useQuery({ demo: { value: 1 } }, { once: true }),
+      {
+        wrapper,
+      },
+    );
+    expect(result.current).toEqual([null, true, null]);
+    await waitForValueToChange(() => result.current[1]);
+    expect(result.current).toEqual([{ demo: { value } }, false, null]);
+    expect(result.error).toBeFalsy();
+  });
+});
