0636f8b9770e60a0d670a04703e4eae3924bea14
Aravind <hi@aravind.rs>
2021-03-12 09:44:40 +0800

    Fix test flakiness

--- src/website/example.test.js
+++ src/website/example.test.js
@@ -2,16 +2,16 @@ import { fork } from 'child_process';
 import puppeteer from 'puppeteer';
 
 const PORT = 1025 + Math.floor(Math.random() * 30000);
-jest.setTimeout(60000);
+jest.setTimeout(120000);
 
 describe('integration', () => {
-  let server;
+  let server, browser;
   beforeAll(() => {
     return Promise.all([
       new Promise((resolve, reject) => {
         server = fork(`${__dirname + '/server.js'}`, {
           env: { PORT },
-          stdio: 'ignore',
+          silent: true,
           execArgv: ['--es-module-specifier-resolution=node'],
         });
         server.on('error', reject);
@@ -20,15 +20,18 @@ describe('integration', () => {
           if (message === 'ready') resolve();
         });
       }),
+      puppeteer
+        .launch({
+          headless: true,
+          // slowMo: 200,
+        })
+        .then((b) => {
+          browser = b;
+        }),
     ]);
   });
 
   async function runExampleTests(url) {
-    const browser = await puppeteer.launch({
-      headless: true,
-      // slowMo: 200,
-    });
-
     const page = await browser.newPage();
     let label;
 
@@ -36,48 +39,64 @@ describe('integration', () => {
     await page.goto(url, { waitUntil: 'networkidle2' });
     expect((await page.$$('.Visitor')).length).toBe(12);
 
+    // console.log('Going to second page');
     // Go to second page and assert.
     await (await page.$('.NextPage')).click();
-    await page.waitForSelector('.Spinner', { hidden: true });
+    // console.log('Clicked; waiting for spinner');
+    await page.waitForSelector('.Spinner', { hidden: true, timeout: 2000 });
     expect((await page.$$('.Visitor')).length).toBe(12);
     label = await (await page.$('.CurrPage')).evaluate((el) => el.textContent);
     expect(label).toMatch(/after/);
 
+    // console.log('Going to third page');
     // Go to third page. This should have around 6 elements.
     await (await page.$('.NextPage')).click();
-    await page.waitForSelector('.Spinner', { hidden: true });
+    // console.log('Clicked; waiting for spinner');
+    await page.waitForSelector('.Spinner', { hidden: true, timeout: 2000 });
     expect((await page.$$('.Visitor')).length).toBeLessThan(12);
 
+    // console.log('Going back to second page');
     // Go back to second page.
     await (await page.$('.PrevPage')).click();
-    await page.waitForSelector('.Spinner', { hidden: true });
+    // console.log('Clicked; waiting for spinner');
+    await page.waitForSelector('.Spinner', { hidden: true, timeout: 2000 });
     expect((await page.$$('.Visitor')).length).toBe(12);
     label = await (await page.$('.CurrPage')).evaluate((el) => el.textContent);
     expect(label).toMatch(/Last.*until/);
 
+    // console.log('Going back to first page');
     // Go back to first page. The page label should flip around.
-    await (await page.$('.PrevPage')).click();
-    await page.waitForSelector('.Spinner', { hidden: true });
-    await (await page.$('.PrevPage')).click();
-    await page.waitForSelector('.Spinner', { hidden: true });
-    expect((await page.$$('.Visitor')).length).toBe(12);
-    label = await (await page.$('.CurrPage')).evaluate((el) => el.textContent);
+    let attempts = 0;
+    do {
+      await (await page.$('.PrevPage')).click();
+      // console.log('Clicked; waiting for spinner');
+      await page.waitForSelector('.Spinner', { hidden: true, timeout: 2000 });
+      // console.log('Waiting for visitor');
+      await page.waitForSelector('.Visitor', { timeout: 2000 });
+      expect((await page.$$('.Visitor')).length).toBe(12);
+      label = await (await page.$('.CurrPage')).evaluate(
+        (el) => el.textContent,
+      );
+      attempts++;
+    } while (!label.includes('First') && attempts < 2);
     expect(label).toMatch(/First/);
     expect(label).not.toMatch(/after/);
 
-    browser.close();
+    await page.close();
   }
 
   const exampleUrl = `http://localhost:${PORT}/learn/10-Full-Example`;
   test('exampleWs', () => runExampleTests(exampleUrl));
   test('exampleHttp', () => runExampleTests(exampleUrl + '?usehttp'));
 
-  afterAll(
-    () =>
+  afterAll(() =>
+    Promise.all([
+      browser.close(),
       new Promise((resolve, reject) => {
         server.kill();
         server.on('exit', resolve);
         server.on('error', reject);
       }),
+    ]),
   );
 });
