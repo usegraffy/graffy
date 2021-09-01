7bbb213354727a6d8f5fb07372fade9f744317ae
Annie Dang <annie.dang@bravoco.io>
2021-09-01 20:00:59 +1000

    move id => args

--- src/simple-pg/test/sql/upsert.test.js
+++ src/simple-pg/test/sql/upsert.test.js
@@ -6,21 +6,21 @@ import { _nowToInt } from '../../sql/helper';
 
 describe('test upset', () => {
   const options = { id: 'id', table: 'post', version: 'version' };
+  const id = 'post22';
   const data = {
-    id: 'post22',
     type: 'post',
     name: 'hello',
     email: 'world',
   };
   test('should return correct upsert fof put sql with right params and values', async () => {
-    const sqlQuery = put(data, options);
+    const sqlQuery = put(data, id, options);
     const version = sqlQuery.values[sqlQuery.values.length - 1];
     expectSql(
       sqlQuery,
       sql`INSERT INTO "post" ("id", "type", "name", "email", "version")
-      VALUES (${data.id}, ${data.type}, ${data.name}, ${data.email}, ${version} )
+      VALUES (${id}, ${data.type}, ${data.name}, ${data.email}, ${version} )
       ON CONFLICT ("id") DO UPDATE SET
-      ("id", "type", "name", "email", "version") = (${data.id}, ${data.type},${data.name}, ${data.email}, ${version})
+      ("type", "name", "email", "version") = (${data.type},${data.name}, ${data.email}, ${version})
       RETURNING *
     `,
     );
