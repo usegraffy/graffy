c0dd3c8efde32f31c8f5a17992edcd20e2323501
Aravind <hi@aravind.rs>
2022-02-03 22:59:08 +0800

    Upgrade packages

--- src/website/services/mockVisitorList.js
+++ src/website/services/mockVisitorList.js
@@ -1,4 +1,4 @@
-import faker from 'faker';
+import faker from '@faker-js/faker';
 import debug from 'debug';
 const log = debug('graffy:website:server');
 import { encodeGraph } from '@graffy/common';
