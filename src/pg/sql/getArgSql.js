3b7302502112707b989f3d09560c7dda272cd1c8
Aravind <hi@aravind.rs>
2022-08-23 16:13:28 +0800

    simplify

--- src/pg/sql/getArgSql.js
+++ src/pg/sql/getArgSql.js
@@ -8,7 +8,7 @@ import { getJsonBuildTrusted, lookup } from './clauses.js';
   Uses the args object (typically passed in the $key attribute)
 
   @param {object} args
-  @param {{prefix: string, idCol: string}} options
+  @param {{prefix: string, idCol: string, verDefault: string}} options
 
   @typedef { import('sql-template-tag').Sql } Sql
   @return {{ meta: Sql, where: Sql[], order?: Sql, group?: Sql, limit: number }}
@@ -20,31 +20,32 @@ export default function getArgSql(
   const { $order, $group, ...filter } = rest;
   const { prefix, idCol } = options;
 
+  const meta = (key) =>
+    $group ? getAggMeta(key, options) : getArgMeta(key, options);
+
+  const hasRangeArg =
+    $before || $after || $since || $until || $first || $last || $all;
+
   if ($order && $group) {
     // TODO: Allow this.
     throw Error('pg_arg.order_and_group_unsupported in ' + prefix);
   }
 
-  const meta = (key) =>
-    $group ? getAggMeta(key, options) : getArgMeta(key, options);
+  if (($order || ($group && $group !== true)) && !hasRangeArg) {
+    throw Error('pg_arg.range_arg_expected in ' + prefix);
+  }
+
+  const baseKey = sql`${JSON.stringify(rest)}::jsonb`;
+  const where = [];
+
+  if (!isEmpty(filter)) where.push(getFilterSql(filter, options));
+
+  if (!hasRangeArg) return { meta: meta(baseKey), where, limit: 1 };
 
   const groupCols =
     Array.isArray($group) && $group.length && $group.map(lookup);
 
   const group = groupCols ? join(groupCols, ', ') : undefined;
-
-  const hasRangeArg =
-    $before || $after || $since || $until || $first || $last || $all || $order;
-
-  let filterKey;
-  const where = [];
-  if (!isEmpty(filter)) {
-    where.push(getFilterSql(filter, options));
-    filterKey = sql`${JSON.stringify(filter)}::jsonb`;
-  }
-
-  if (!hasRangeArg && !$group)
-    return { meta: meta(filterKey), where, limit: 1 };
 
   const orderCols = ($order || [idCol]).map((orderItem) =>
     orderItem[0] === '!'
@@ -69,28 +70,14 @@ export default function getArgSql(
       `, `,
     );
 
-  const orderKey =
-    $order &&
-    getJsonBuildTrusted({ $order: sql`${JSON.stringify($order)}::jsonb` });
+  const cursorKey = getJsonBuildTrusted({
+    $cursor:
+      $group === true
+        ? sql`''`
+        : sql`jsonb_build_array(${join(groupCols || orderCols)})`,
+  });
 
-  const cursorKey =
-    $group === true && !hasRangeArg
-      ? undefined
-      : getJsonBuildTrusted({
-          $cursor:
-            $group === true
-              ? sql`''`
-              : sql`jsonb_build_array(${join(groupCols || orderCols)})`,
-        });
-
-  const groupKey =
-    $group &&
-    getJsonBuildTrusted({ $group: sql`${JSON.stringify($group)}::jsonb` });
-
-  const key = sql`(${join(
-    [filterKey, groupKey, orderKey, cursorKey].filter(Boolean),
-    ` || `,
-  )})`;
+  const key = sql`(${baseKey} || ${cursorKey})`;
 
   return {
     meta: meta(key),
