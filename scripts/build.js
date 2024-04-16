9bbdfbd0ffee18d02d8c3ecb7110fcbd351d28bb
Aravind <hi@aravind.rs>
2024-04-16 18:59:25 +0800

    make package script support node: imports

--- scripts/build.js
+++ scripts/build.js
@@ -7,7 +7,8 @@ import { dst, ownPattern, read, src } from './utils.js';
 const depPattern = /^[^@][^/]*|^@[^/]*\/[^/]*/;
 
 // ESM-only deps are built into the bundle rather than
-// keeping them external, to prevent
+// keeping them external, to prevent installation errors
+// in commonJS projects.
 const esmOnlyDeps = ['sql-template-tag', 'nanoid'];
 
 export default async function build(name, version, watch, onUpdate) {
@@ -113,7 +114,7 @@ export default async function build(name, version, watch, onUpdate) {
           dependencies[dep] = version;
         } else if (depVersions[dep]) {
           dependencies[dep] = depVersions[dep];
-        } else if (builtinModules.includes(dep)) {
+        } else if (builtinModules.includes(dep) || dep.startsWith('node:')) {
           console.log(`INFO [${name}] ignoring built-in ${dep}`);
         } else {
           console.warn(`WARN [${name}] unversioned package ${dep}`);
