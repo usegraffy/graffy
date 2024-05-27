ef47e7610f1acda688f83d35292389f3daa3c743
Aravind <hi@aravind.rs>
2024-05-27 14:01:07 +0800

    Remove debug logs

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,16 +2,14 @@ import { npm } from './utils.js';
 
 export default async function publish(name, version) {
   const isPre = version.includes('alpha') || version.includes('beta');
-  console.log('Called publish', isPre);
   try {
-    const res = await npm(
+    await npm(
       name,
       'publish',
       '--access',
       'public',
       ...(isPre ? ['--tag', 'pre'] : []),
     );
-    console.log('Publish result', res);
     console.log(`INFO [${name}] published`);
   } catch (e) {
     console.error(`ERROR [${name}] publishing failed`);
