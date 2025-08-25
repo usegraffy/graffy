d0dcd40526579fc31e0f847764a66da4fb210d9f
Chow Loong Jin <hyperair@debian.org>
2025-08-25 19:15:58 +0800

    pg: Rewrite sql formatter to support more types

--- src/pg/sql/format.js
+++ src/pg/sql/format.js
@@ -1,27 +1,42 @@
+import { escapeLiteral } from 'pg';
 import { format } from 'sql-formatter';
+
+/**
+ * @param {any} value
+ * @returns {string}
+ */
+function formatSqlParam(value) {
+  if (value === null || value === undefined) return 'null';
+  if (['number', 'boolean'].includes(typeof value)) return value.toString();
+
+  switch (typeof value) {
+    case 'number':
+    case 'boolean':
+      return value.toString();
+
+    case 'string':
+      return escapeLiteral(value);
+
+    case 'object':
+      if (Array.isArray(value))
+        return `array[${value.map(formatSqlParam).join(', ')}]`;
+      return `'${JSON.stringify(value)}'`;
+
+    default:
+      // shouldn't be reached
+      return '';
+  }
+}
 
 /**
  * @param {import('sql-template-tag').Sql} sql
  * @returns {string}
  */
 export default function formatSql(sql) {
-  const strings = sql.strings.slice(0);
-  const values = sql.values.slice(0);
-  const output = [];
-  while (strings.length) {
-    output.push(strings.shift());
-
-    if (!values.length) break;
-    const value = values.shift();
-    output.push(
-      typeof value === 'number'
-        ? value.toString()
-        : value === null
-          ? 'null'
-          : typeof value === 'object'
-            ? `'${JSON.stringify(value)}'`
-            : `'${value}'`,
-    );
-  }
-  return format(output.join(''), { language: 'postgresql' });
+  return format(sql.text, {
+    language: 'postgresql',
+    params: Object.fromEntries(
+      sql.values.map((value, idx) => [idx + 1, formatSqlParam(value)]),
+    ),
+  });
 }
