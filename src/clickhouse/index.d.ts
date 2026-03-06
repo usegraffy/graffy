1b085a58973edaf58908d246cbd97a7e28bcf066
Baqir Khan <baqirkhan00@gmail.com>
2026-03-06 11:31:40 +0530

    add index.d.ts

---
+++ src/clickhouse/index.d.ts
@@ -0,0 +1,31 @@
+import type Graffy from '@graffy/core';
+
+export type ClickhouseJoinOptions = {
+    table?: string;
+    idCol?: string;
+    verCol?: string;
+    schema?: unknown;
+    database?: string;
+    final?: boolean;
+    refCol?: string;
+    joins?: Record<string, ClickhouseJoinOptions>;
+};
+
+export type ClickhouseOptions = {
+    table?: string;
+    idCol?: string;
+    verCol?: string;
+    schema?: unknown;
+    database?: string;
+    final?: boolean;
+    joins?: Record<string, ClickhouseJoinOptions>;
+    connection?: unknown;
+};
+
+export type ClickhouseProvider = (store: Graffy) => void;
+
+export declare const clickhouse: (
+    options?: ClickhouseOptions,
+) => ClickhouseProvider;
+
+export declare const ch: typeof clickhouse;
