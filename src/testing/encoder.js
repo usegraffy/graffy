27bca5baa91a62c9c106465654d956b06bcd9a80
Aravind <hi@aravind.rs>
2026-03-27 19:44:05 +0800

    Fix broken import

--- src/testing/encoder.js
+++ src/testing/encoder.js
@@ -1,4 +1,4 @@
-import { addStringify, encodeValue } from '../graffy/common';
+import { addStringify, encodeValue } from '../common';
 
 /** @type {Record<string, any>} */
 export const e = new Proxy(
